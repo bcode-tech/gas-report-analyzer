9310c8acba6525b31aaa8216fb783a4766a10f35
mtumiati <manuel.tumiati@knobs.it>
2021-02-23 09:26:10 +0100

    UPDATE: parametric footer image or text

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,19 +1,32 @@
 import { ExternalLinkIcon } from "@chakra-ui/icons";
-import { Box, Container, Text, Center, Link } from "@chakra-ui/react";
+import { Box, Container, Text, Center, Link, Image } from "@chakra-ui/react";
 
-export const Footer = ({ title }) => (
+const footerPoweredBy = process.env.REACT_APP_FOOTER_POWERED_BY;
+const footerLink = process.env.REACT_APP_FOOTER_LINK;
+const footerLogoURL = process.env.REACT_APP_FOOTER_LOGO;
+
+export const Footer = () => (
   <Box h={50} w="100%" bg="brand.800">
     <Container maxW="6xl" h="100%">
       <Center h="100%">
         <Text color="brand.700">Powered by&nbsp;</Text>
-        <Link
-          href="https://bcode.cloud"
-          isExternal
-          color="brand.900"
-          fontFamily="Barlow"
-        >
-          BCode
-        </Link>
+        {footerLogoURL ? (
+          <Image
+            src={footerLogoURL}
+            h={5}
+            onClick={() => window.open(footerLink, "_blank")}
+            cursor="pointer"
+          />
+        ) : (
+          <Link
+            href={footerLink}
+            isExternal
+            color="brand.900"
+            fontFamily="Barlow"
+          >
+            {footerPoweredBy}
+          </Link>
+        )}
       </Center>
     </Container>
   </Box>
